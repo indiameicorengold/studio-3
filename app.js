929eb70cc32fdfc741bbdcc1f9c10065bf4ed929
indiameicorengold <indiameicorengold@gmail.com>
2022-09-29 18:44:05 -0400

    studio 3 done!

--- app.js
+++ app.js
@@ -26,14 +26,25 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
+  //console.log(fruitsArray);
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15",
 
+  const sweetFruits = fruitsArray.filter(fruit => fruit.nutritions.sugar > 15.0);
+  console.log(sweetFruits);
+
   // TODO: Create a new HTML element to display your data
+  const newElement = document.createElement('div');
+  newElement.className = "fruits";
+  sweetFruits.forEach(function(entry) {
+    newElement.innerHTML += entry.name + " ";
+  });
+
+  
 
   // TODO: Append your new element to the page
+  gallery.append(newElement);
 
 }
 
